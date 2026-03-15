883dc4187ea18f433c60d776b642d90574a10a84
adri_casa <atenag10@educarex.es>
2026-03-15 02:04:19 +0100

    Revert "Limpiar rama SergioS"
    This reverts commit b0041bf236614c4cd1271b2b0a6e9ef254c19f51.

---
+++ tests/MainTest.java
@@ -0,0 +1,33 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+class MainTest {
+
+    @Test
+    @DisplayName("Test ejecución del programa principal")
+    void testMain() {
+
+        PrintStream salidaConsola = System.out;
+
+        ByteArrayOutputStream salida = new ByteArrayOutputStream();
+        PrintStream salidaTest = new PrintStream(salida);
+
+        System.setOut(salidaTest);
+
+        try {
+            Main.main(new String[]{});
+        } catch (Exception e) {
+            fail("El programa lanzó una excepción inesperada");
+        }
+
+        System.setOut(salidaConsola);
+
+        String output = salida.toString();
+
+        assertTrue(output.contains("INICIO"), "El programa no se inicia correctamente");
+    }
+}
